18b2c1b70cbfdd6759dec074bd2801bf99e0c9f6
mauer1@tlu.ee <JaanusLille>
2021-09-06 11:06:46 +0300

    React start

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Item from './components/Item.js';
+
+function App() {
+  return (
+    <div>
+      Tere
+      <Item />
+      <Item />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Item.js
@@ -0,0 +1,11 @@
+function Item() {
+    return (
+        <div>
+            <div className="itemName">Ese nr1</div>
+            <div className="itemName">100</div>
+            <div className="itemName">mobiles</div>
+        </div>
+    )
+}
+
+export default Item;
