4dabb0856d9d3315e470de8b0313e4a719e06bbb
jaanu <JaanusLille@users.noreply.github.com>
2021-10-06 13:03:00 +0300

    reorganization 2

--- Raimo/frontend-react/src/components/Greeting.js
+++ Raimo/frontend-react/src/components/Greeting.js
@@ -1,11 +1,12 @@
-import React from "react" 
+import React from "react"
 
 const Greeting = ({ name, age }) => {
-    return(
-        <>
-            <h1>Greeting { name }</h1>
-            <p>Greeting text { age }</p>
-        </>
-    )
+  return (
+    <>
+      <h1>Greeting { name }</h1>
+      <p>Greeting text { age }</p>
+    </>
+  )
 }
+
 export default Greeting
